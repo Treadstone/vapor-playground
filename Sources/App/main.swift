b29c9a9e1e499f69c786c59040b3d1befd976543
AgtLucas <lucas@agtlucas.com>
2016-10-06 19:05:03 -0300

    From 4 to 2 spaces

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,10 +3,10 @@ import Vapor
 let drop = Droplet()
 
 drop.get { req in
-    let lang = req.headers["Accept-Language"]?.string ?? "en"
-    return try drop.view.make("welcome", [
-    	"message": Node.string(drop.localization[lang, "welcome", "title"])
-    ])
+  let lang = req.headers["Accept-Language"]?.string ?? "en"
+  return try drop.view.make("welcome", [
+    "message": Node.string(drop.localization[lang, "welcome", "title"])
+  ])
 }
 
 drop.resource("posts", PostController())
